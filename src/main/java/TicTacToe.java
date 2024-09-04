8af5f01c1ed3badb9f766244eaf7a5db3e4b68e0
sandyrabiaza <sandy.rabiaza@gmail.com>
2024-09-04 15:43:04 +0200

    add test

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class TicTacToe {
+
+    public Boolean isOver() {
+        return false;
+    }
 }
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TicTacToeTest {
+
+    @Test
+    public void a_new_game_is_not_over() {
+        TicTacToe newGame = new TicTacToe();
+
+        assertThat(newGame.isOver()).isFalse() ;
+    }
+
+    /*
+    -1 player take 1 column
+    -1 player take 1 row
+    -1 player take 1 diagonale
+    -1 player can't play twice in a row
+    -1 player can't play on an unavailable case
+     */
+
 }
